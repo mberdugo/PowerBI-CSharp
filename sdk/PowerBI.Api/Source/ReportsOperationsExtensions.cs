15ca2901869afc9554a7ff610dceef8e7d380458
Mona Berdugo <82447518+mberdugo@users.noreply.github.com>
2021-10-27 15:36:11 +0300

    Story 1825960 Embedtoken view access clarification

--- sdk/PowerBI.Api/Source/ReportsOperationsExtensions.cs
+++ sdk/PowerBI.Api/Source/ReportsOperationsExtensions.cs
@@ -1650,9 +1650,9 @@ namespace Microsoft.PowerBI.Api
             /// Dataset.Read.All&lt;/li&gt;&lt;li&gt;Content.Create - required only if
             /// allowSaveAs specified in
             /// [GenerateTokenRequest](/rest/api/power-bi/embedtoken/reports_generatetokeningroup#GenerateTokenRequest)&lt;/li&gt;&lt;/ul&gt;Note
-            /// that when `accessLevel` is set to `View`, the client can still go into edit
-            /// mode, but they can’t save any changes.&lt;br/&gt;To set the permissions
-            /// scope, see [Register an
+            /// that when `accessLevel` is set to `View`, the client can go into edit mode,
+            /// but they can’t save any changes.&lt;br/&gt;To set the permissions scope,
+            /// see [Register an
             /// app](https://docs.microsoft.com/power-bi/developer/register-app).&lt;br/&gt;When
             /// using service principal for authentication, refer to [Service Principal
             /// with Power
@@ -1695,9 +1695,9 @@ namespace Microsoft.PowerBI.Api
             /// Dataset.Read.All&lt;/li&gt;&lt;li&gt;Content.Create - required only if
             /// allowSaveAs specified in
             /// [GenerateTokenRequest](/rest/api/power-bi/embedtoken/reports_generatetokeningroup#GenerateTokenRequest)&lt;/li&gt;&lt;/ul&gt;Note
-            /// that when `accessLevel` is set to `View`, the client can still go into edit
-            /// mode, but they can’t save any changes.&lt;br/&gt;To set the permissions
-            /// scope, see [Register an
+            /// that when `accessLevel` is set to `View`, the client can go into edit mode,
+            /// but they can’t save any changes.&lt;br/&gt;To set the permissions scope,
+            /// see [Register an
             /// app](https://docs.microsoft.com/power-bi/developer/register-app).&lt;br/&gt;When
             /// using service principal for authentication, refer to [Service Principal
             /// with Power
